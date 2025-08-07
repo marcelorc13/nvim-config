f74308bedea51246ed8bc25ee21c295117658387
Marcelo Ramalho <marceloramalho@localhost.localdomain>
2025-08-07 17:09:54 -0300

    adicionando vim tree

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,31 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit...\n" },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit...\n" },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "  -- Agora posicionado corretamente
+vim.g.mapleader = " " -- Agora posicionado corretamente
 
 require("lazy").setup({
-  require("plugins.lsp"),
-  require("plugins.colorscheme"),
-  require("plugins.mini"),
-  require("plugins.cmp"),
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+	require("plugins.lsp"),
+	require("plugins.colorscheme"),
+	require("plugins.mini"),
+	require("plugins.cmp"),
+	require("plugins.nvim-tree"),
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	}
 })
-
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,46 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+		float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.o.columns
+            local screen_h = vim.o.lines
+            return {
+              relative = "editor",
+              border = "rounded",
+              width = screen_w,
+              height = screen_h - 2,
+              row = 1,
+              col = 0,
+            }
+          end,
+	},
+        width = 30,
+      },
+      renderer = {
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              default = "",
+              open = "",
+            },
+          },
+        },
+      },
+      diagnostics = {
+        enable = true,
+      },
+    })
+
+    -- Keybind para abrir o explorer com <leader>e
+    vim.keymap.set("n", "<leader>je", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+  end,
+}
