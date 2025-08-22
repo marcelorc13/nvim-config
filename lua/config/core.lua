768aa57cf38118bb03b23880e7fefd5e744f61aa
Marcelo Ramalho <marcelorcarnauba@gmail.com>
2025-08-22 18:03:42 -0300

    edicao da status line

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -1,9 +1,13 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
+vim.opt.colorcolumn = "90,120,150"
+
 vim.opt.cursorline = true
+vim.opt.guicursor = "a:block"
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = "yes"
 
 vim.opt.winborder = "rounded"
 vim.opt.tabstop = 4
@@ -12,7 +16,7 @@ vim.opt.smartindent = true
 vim.opt.autoindent = true
 
 vim.opt.backup = false
-vim.opt.writebackup= false
+vim.opt.writebackup = false
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.autoread = true
@@ -23,7 +27,16 @@ vim.opt.mouse = "a"
 vim.opt.encoding = "UTF-8"
 vim.opt.clipboard:append("unnamedplus")
 
-vim.opt.ignorecase = true    
-vim.opt.smartcase = true                          
-vim.opt.hlsearch = false 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+local modeShower =
+"%{mode() == 'n' ? 'N' : mode() == 'i' ? 'I' : mode() == 'i' ? 'I' : mode() == 'c' ? 'C' : mode() == 't' ? 'TERM' : mode()} "
+
+vim.opt.statusline = modeShower .. " %f  %= %y"
+
+
+vim.api.nvim_set_hl(0, "StatusLine", { bg= "#1d2021" })
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,6 @@ vim.g.maplocalleader = " "
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n',  '<leader>jv', '<cmd> Ex <CR>', opts)
-
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
