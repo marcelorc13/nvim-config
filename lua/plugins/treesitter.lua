3644a24b9c0c673e2488144a52ca9315eca50162
Marcelo Ramalho <marcelorcarnauba@gmail.com>
2026-02-05 10:54:33 -0300

    removing markview plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-	dependencies = { "OXY2DEV/markview.nvim" },
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
